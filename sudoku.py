b7512ddfa4a675799dc63abaf59196910934151e
Andrew Mauricio <amauricio89@gmail.com>
2015-01-26 00:13:12 -0800

    Add function docstrings

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,5 @@
 import random
 import sys
-from copy import deepcopy
 
 max_att = 120
 difficulty_table = {
@@ -10,12 +9,22 @@ difficulty_table = {
 }
 
 def sudoku_generator(diff='easy'):
+    '''Print a newly generated Sudoku Puzzle
+
+    Args:
+        diff: difficulty of puzzle: 'easy, 'medium', 'hard' (default 'easy')
+    '''
     solved = solved_generator()
     remove_amount = get_difficulty(diff)
     remove_cells(solved, remove_amount)
     print solved
 
 def solved_generator():
+    '''Generate a valid and solved Sudoku Puzzle
+
+    Returns:
+        a list of nine lists that compose a completed Sudoku puzzle
+    '''
     board = [['_' for row in xrange(9)] for col in xrange(9)]
     for row in xrange(9):
         for col in xrange(9):
@@ -33,6 +42,15 @@ def solved_generator():
     return board
 
 def generate_box_list(board, cr, cc):
+    '''Generates a list of current values in a current box
+
+    Args:
+        board: current Sudoku board
+        cr: current row index
+        cc: current column index
+    Returns:
+        a list of nine values
+    '''
     b = []
     curr_box_row = int(cr/3)
     curr_box_col = int(cc/3)
@@ -42,6 +60,14 @@ def generate_box_list(board, cr, cc):
     return b
 
 def remove_cells(board, num_cells):
+    '''Generates an unsolved Sudoku from a solved one
+
+    Args:
+        board: current Sudoku board
+        num_cells: amount of cells to be removed from the puzzle
+    Returns:
+        a list of nine lists that compose an uncomplete Sudoku puzzle
+    '''
     if num_cells != 0:
         rand_col = random.randint(0,8)
         rand_row = random.randint(0,8)
@@ -55,9 +81,22 @@ def remove_cells(board, num_cells):
 
 
 def in_peers(v, cr, cc, sm):
+    '''Checks if value v is in the current row, column, or appropriate box
+
+    Args:
+        v: value to be checked
+        cr: current row index
+        cc: current column index
+        sm: current box (sub-matrix)
+    '''
     return (v in cr or v in cc or v in sm)
 
 def get_difficulty(d):
+    '''Returns a difficulty value 
+
+    Args:
+        d: difficulty keyword to be looked up
+    '''
     return difficulty_table[d] if difficulty_table.has_key(d) else 20
 
 if __name__ == '__main__':
